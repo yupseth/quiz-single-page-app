156bbb93e440e38b3ee9695c8cf1e8d49c0c00de
yupseth <cristinayup@gmail.com>
2023-01-24 03:15:40 +0200

    get filtered questions

--- script.js
+++ script.js
@@ -4,15 +4,23 @@
 const content = document.querySelector(".content");
 const dropdown = document.querySelector("select");
 const startButton = document.querySelector(".button-start");
+const resetButton = document.querySelector(".reset-wrapper");
 const playScreen = document.querySelector(".play-screen");
+const difficultyRadioButtons = document.getElementsByName("difficulty");
 
 // GLOBAL DECLARATIONS
 let categories = [];
+let isLoading = false;
+let selectedCategory;
+let selectedDifficulty;
+let apiData;
+let apiDataResults;
 
 // HELPERS
 const toggleHidden = function () {
   content.classList.add("hidden");
   playScreen.classList.remove("hidden");
+  resetButton.classList.remove("hidden");
 };
 
 // CATEGORY DROPDOWN
@@ -28,6 +36,44 @@ fetch("https://opentdb.com/api_category.php")
     });
   });
 
+// READ CATEGORY AND DIFFICULTY
+const readSelectedCategory = () => {
+  for (const option of dropdown.options) {
+    if (option.selected) {
+      selectedCategory = option.value;
+    }
+  }
+};
+
+const readSelectedDifficulty = () => {
+  for (const element of difficultyRadioButtons) {
+    if (element.checked) {
+      selectedDifficulty = element.value;
+    }
+  }
+};
+
+// GET API DATA
+const getQuestions = () => {
+  isLoading = true;
+  fetch(
+    `https://opentdb.com/api.php?amount=10&category=${selectedCategory}&difficulty=${selectedDifficulty}&type=multiple`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      apiData = data;
+      apiDataResults = apiData.results;
+      console.log(apiDataResults);
+    })
+    .finally(() => {
+      isLoading = false;
+    });
+};
+
+// EVENTS
 startButton.addEventListener("click", () => {
   toggleHidden();
+  readSelectedCategory();
+  readSelectedDifficulty();
+  getQuestions();
 });
